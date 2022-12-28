41a6c53f08b7236a533c29bdff13443713d7bc22
Vidhi Kapadia <vidhikap27@gmail.com>
2022-12-28 17:46:20 +0530

    removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 
 interface IPokemon {
   id: number;
